28b8ffb741cd2ff889364eaebf1ae07ab7ecfdf2
Liam McLoughlin <hexxeh@hexxeh.net>
2015-03-11 23:43:49 +0000

    Use Git properly for source

--- libcrippy.rb
+++ libcrippy.rb
@@ -1,8 +1,7 @@
 class Libcrippy < Formula
   homepage ""
-  url "https://github.com/Hexxeh/libcrippy/archive/8f4fea274a7e644daae62cdf50e77d565837778b.zip"
+  url "https://github.com/Hexxeh/libcrippy.git", :using => :git, :revision => "8f4fea2"
   version "1"
-  sha1 "c56cc62edcceb5ddbfe186ac0a22c2f1ce0672ea"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
--- libmbdb.rb
+++ libmbdb.rb
@@ -1,8 +1,7 @@
 class Libmbdb < Formula
   homepage ""
-  url "https://github.com/Hexxeh/libmbdb/archive/5996ec53efccb499593ab2e1ab8dfde8ec914b6d.zip"
+  url "https://github.com/Hexxeh/libmbdb.git", :using => :git, :revision => "5996ec5"
   version "1"
-  sha1 "5f47958463af10c6d6924a8e7671484f83b09b5a"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
